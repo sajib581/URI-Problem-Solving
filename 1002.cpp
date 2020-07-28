2352b08db26be0f64bb59f6ec0ef87950eb6d786
sajib581 <sajibdas581@gmail.com>
2020-07-28 20:11:04 +0600

    URI Solving Start

---
+++ 1002.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include <iostream>
+using namespace std ;
+
+int main(){
+    double R,A,pie ;
+    cin>>R ;
+    pie = 3.14159 ;
+
+    A = pie*R*R ;
+
+    printf("A=%.4f\n",A) ;
+
+    return 0 ;
+
+}
+
---
+++ 1003.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std ;
+
+int main(){
+    int A,B,SOMA ;
+    cin>>A>>B ;
+    SOMA = A+B ;
+    cout<<"SOMA = "<<SOMA<<"\n" ;
+
+    return 0 ;
+
+}
